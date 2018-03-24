35ce152254308761a89014a9784de8c290713be8
Stef Tweepenninckx <stef.tweepenninckx@student.kuleuven.be>
2018-03-24 15:41:23 +0100

    quicksort

--- Practicum 1/src/gna/QuickSort.java
+++ Practicum 1/src/gna/QuickSort.java
@@ -1,20 +1,48 @@
+
 package gna;
 
 /**
  * Performs sort by using the Quick Sort algorithm.
- *
+ *TODO mogelijk verwijderen abstract, zie na tests
  */
-public class QuickSort extends SortingAlgorithm{
+public abstract class QuickSort extends SortingAlgorithm{
 	/**
 	 * Sorts the given array using quick sort.
 	 * 
 	 * @see super
 	 */
-	public long sort(Comparable[] array) throws IllegalArgumentException {
+	public long sort(Comparable[] array, int low, int high) throws IllegalArgumentException {
 		if (array == null) {
 			throw new IllegalArgumentException("argument 'array' must not be null.");
 		}
-		throw new RuntimeException("not implemented"); // TODO
+		if (high <= low) return 0;
+		int k = partition(array, low, high);
+		sort(array, low, k -1);
+		sort(array, k + 1, high);		
+		return counter;
+	}
+	
+	static long counter = 0;
+	
+	private static int partition(Comparable[] array, int low, int high){
+		int i = low + 1;
+		int j = high;
+		Comparable pivot = array[low];
+		while (true){
+			while (less(array[i++], pivot)){
+				counter++;
+				if (i == high) break;
+			}
+			
+			while (less(array[j--], pivot)){
+				counter++;
+				if (j == low) break;
+			}
+			if (i>=j) break;
+			swap(array, i, j);
+		}
+		swap(array, j, low);
+		return j;
 	}
 
 	/**
